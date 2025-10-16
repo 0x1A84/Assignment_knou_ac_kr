38783cea0148da77ef940d42d180f79323efb1b2
Jason Parker <0x1a84ff@gmail.com>
2025-10-17 04:03:10 +0900

    파일명 변경

--- Assignment_Data_Analysis_with_Open_Source/main.py
+++ Assignment_Data_Analysis_with_Open_Source/main.py
@@ -1,12 +1,33 @@
 import pandas as pd
-from import_api_test_2 import get_personal_energy_usage_data
+import numpy as np
+import os
+from api_scrapping import get_personal_energy_usage_data
 
 # main.py
-energy_usage = pd.DataFrame(columns = ['전기', '가스', '수도', '지역난방', '연도', '계절'])
+df = pd.DataFrame()
 
-responses = get_personal_energy_usage_data()
+def interpolate_csv(df_cleaned):
+    for col in ['CNT', 'GUS']:
+        df_cleaned[col] = df_cleaned[col].fillna(df_cleaned[col].interpolate(method = 'spline', order = 3))
+    print(df_cleaned[['CNT', 'GUS']].isnull().sum())
+    print(df_cleaned.head())
+    return df_cleaned
 
-df = pd.DataFrame(responses)
-print(df.head())
+if os.path.isfile('energy.csv'):
+    df = pd.read_csv('./energy.csv', header=0)
+    print(df.isnull().sum())
+    df_cleaned = interpolate_csv(df)
+    print(df[['CNT', 'EUS', 'GUS', 'HUS']])
+    
+else:
+    responses = get_personal_energy_usage_data()
+    df = pd.DataFrame(responses)
+    print(df)
+
+
+
+
+
+
 
 df.to_csv("energy.csv")
